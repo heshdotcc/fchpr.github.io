ddadbd08cbe4f302c8568b9ca8de62e9f43621f1
fchpr <sietedosfede@gmail.com>
2019-07-15 18:34:29 -0300

    Use openlayers instead mapbox

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,51 +1,44 @@
 <script>
-    import { onMount, afterUpdate } from 'svelte';
-    let container, map, mapboxgl, lat=-34.63020489294104, lon=-58.45173159155483, zoom=14.5;
-    onMount(async () => {
-/* 
-		const link = document.createElement('link');
-		link.rel = 'stylesheet';
-        link.href = 'https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css';
- */
-        const module = await import('mapbox-gl');
-        mapboxgl = module.default;
-        mapboxgl.accessToken = 'pk.eyJ1Ijoic2lldGVkb3NmZWRlIiwiYSI6ImNqdzVzb3gwYjEyY3k0NHMwMW9tcjRsNjcifQ.eaOFOH0I1JRVyxmMqfqWNA';
-        map = new mapboxgl.Map({
-            container,
-            style: 'mapbox://styles/mapbox/streets-v9',
-            center: [lon, lat],
-            zoom
-        });
-        map.on('mousemove', function (e) {
-            console.log(e);
-/*             console.log(e.lngLat);
-            console.log(e.lngLat); */
-        });
+  import { onMount, afterUpdate } from 'svelte';
+  onMount(async () => {
+    let Map = (await import('ol/Map')).default,
+      View = (await import('ol/View')).default,
+      TileLayer = (await import('ol/layer/Tile')).default,
+      XYZ = (await import('ol/source/XYZ')).default;
+    new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
     });
+  });
+  export let componentName = 'Map'
 </script>
 
-<style>
-    @import './mapbox-gl.css';
-    .mapboxgl-map {
-        width: 100% !important;
-        height: 100% !important;
-    }
-    .mapbox-logo {
-        display: none !important;
-    }
-    .leaflet-bottom, .leaflet-top {
-        z-index: 0 !important;
-        position: absolute;
-        pointer-events: none;
-    }
-</style>
-<!-- 
 <svelte:head>
-    <link rel='stylesheet' href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css'/>
+  <link rel="stylesheet" href="https://openlayers.org/en/latest/css/ol.css" type="text/css">
 </svelte:head>
- -->
-<div bind:this={container} class="mapboxgl-map">
-    {#if map}
-        <slot></slot>
-    {/if}
-</div>
+
+<div id="map"/>
+
+<style>
+  :global(main) {
+    margin: unset !important;
+    max-width: unset !important;
+    padding: unset !important;
+    height: 100%;
+  }
+  #map {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+  }
+</style>
